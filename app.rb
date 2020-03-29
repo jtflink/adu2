0adf8b380f1fa0c0271c216d0f58102fdd59a6ee
jtflink <jordan.flink@kellogg.northwestern.edu>
2020-03-29 21:24:00 +0000

    Update

--- app.rb
+++ app.rb
@@ -1,27 +1,7 @@
-# Set up for the application and database. DO NOT CHANGE. #############################
-require "sinatra"                                                                     #
-require "sinatra/reloader" if development?                                            #
-require "sequel"                                                                      #
-require "logger"                                                                      #
-require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
-require "geocoder"                                                                    #
-connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
-DB ||= Sequel.connect(connection_string)                                              #
-DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
-def view(template); erb template.to_sym; end                                          #
-use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
-before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
-after { puts; }                                                                       #
-#######################################################################################
-
-places_table = DB.from(:places)
-recommendations_table = DB.from(:recommendations)
-users_table = DB.from(:users)
-
-before do
-    @current_user = users_table.where(id: session["user_id"]).to_a[0]
-end
+require "sinatra"
+require "sinatra/reloader"
+def view(template); erb template.to_sym; end
+before { puts "Parameters: #{params}" }
 
 get "/" do
     view "start"
