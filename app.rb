558e64158a0b3cb61452ff8e7d0a0abaf1fd6357
jtflink <jordan.flink@kellogg.northwestern.edu>
2020-04-05 00:45:25 +0000

    Update

--- app.rb
+++ app.rb
@@ -24,3 +24,53 @@ get "/adutype" do
         view "hometype_ne"
     end
 end
+
+get "/existingadu" do
+        view "existingadu"
+end
+
+get "/adutypeback" do
+    view "adutype"
+end
+
+get "/parking" do
+    if params["existingadu"] == "2"
+        view "parking"
+    else
+        view "existingadu_ne"
+    end
+end
+
+get "/eligible" do
+    if params["parking"] == "1"
+        view "eligible"
+    elsif params["parking"] == "2"
+        view "eligible"
+    elsif params["parking"] == "3"
+        view "eligible"
+    elsif params["parking"] == "1" && params["parking"] == "2"
+        view "eligible"
+    elsif params["parking"] == "2" && params["parking"] == "3"
+        view "eligible"
+    elsif params["parking"] == "1" && params["parking"] == "2" && params["parking"] == "3"
+        view "eligible"
+    else
+        view "parking2"
+    end
+end
+
+get "/parkingback" do
+    view "parking"
+end
+
+get "/parking2back" do
+    view "parking2"
+end
+
+get "/eligible2" do
+    if params["parking2"] == "1"
+        view "eligible"
+    else
+        view "parking_ne"
+    end
+end
